379652b38fff97ead108d541c69ef542445c79f0
ya_ron <dr.likle@gmail.com>
2018-12-10 21:12:54 +0200

    Fixed router for gitHub pages.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename = {process.env.PUBLIC_URL + '/'} >
+    <BrowserRouter basename = {'CountryForLife/'} >
       <App />
     </BrowserRouter>
   </Provider>
