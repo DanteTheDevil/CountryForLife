baa72fba653a9034850c10af957c595754fcf77c
ya_ron <dr.likle@gmail.com>
2018-12-18 18:47:36 +0200

    Added constants to result actions

--- src/actions/result.js
+++ src/actions/result.js
@@ -1,19 +1,21 @@
+import * as actionTypes from '../constants/result';
+
 export const getData = countries => {
   return {
-    type: 'RESULT_GET_DATA',
+    type: actionTypes.RESULT_GET_DATA,
     payload: countries
   };
 };
 
 export const updateData = data => {
   return {
-    type: 'RESULT_UPDATE_DATA',
+    type: actionTypes.RESULT_UPDATE_DATA,
     payload: data
   };
 };
 
 export const removeAll = () => {
   return {
-    type: 'RESULT_DELETE_ALL'
+    type: actionTypes.RESULT_DELETE_ALL
   };
 };
